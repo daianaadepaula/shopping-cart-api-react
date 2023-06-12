44cccc50e6209191d4091483030370c730de7c0c
Daiana de Paula <daianaadepaula1@gmail.com>
2023-06-12 09:37:57 -0300

    feat: style product card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header/header';
+import Products from './components/Products/products';
 
 function App() {
   return (
     <>
       <Header />
+      <Products />
     </>
   );
 }
--- src/components/ProductCard/productCard.jsx
+++ src/components/ProductCard/productCard.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import {BsFillCartPlusFill} from 'react-icons/bs';
 import { Section } from '../ProductCard/productCardStyle';
 
 export default function ProductCard() {
   return (
     <Section>
-      <section className="product-card">
-       Product card
+      <section className="product-card">       
+		
+        <img src="http://http2.mlstatic.com/D_739807-MLA46114620155_052021-W.jpg" 
+          alt="produto" 
+          className="card__image" />
+
+        <div className="card__infos">
+          <h2 className="card__price">R$ 200,20</h2>
+          <h2 className="card__title">Iphone</h2>
+        </div>
+
+        <button type="button" className="button__add-cart">
+          <BsFillCartPlusFill />
+        </button>
+
       </section>
     </Section> 
   );
