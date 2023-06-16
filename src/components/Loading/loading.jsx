fd68ecc4ea15567d944bc9d74f83f5cd32cec694
Daiana de Paula <daianaadepaula1@gmail.com>
2023-06-16 10:41:50 -0300

    loading

---
+++ src/components/Loading/loading.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {BiLoader} from 'react-icons/bi';
+import './loading.css';
+
+export default function Loading(){
+  return  <BiLoader className="loading" />;  
+}
--- src/components/Products/products.jsx
+++ src/components/Products/products.jsx
@@ -2,25 +2,29 @@ import React, {useEffect, useState} from 'react';
 import { Section } from './productsStyle';
 import fetchProducts from '../../api/fetchProducts';
 import ProductCard from '../ProductCard/productCard';
+import Loading from '../Loading/loading';
 
 
 export default function Products() {
 
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() =>{
     fetchProducts('iphone').then((response)=>{
       setProducts(response);
+      setLoading(true);
     });
   },[]);
 
-  return (    
-    <Section>
-      <section className="products container">
-        {
-          products.map((product) => <ProductCard key={product.id} data={product} />)
-        }
-      </section>
-    </Section>    
+  return (
+    (loading ? <Loading /> : 
+      <Section>
+        <section className="products container">
+          {products.map((product) => <ProductCard key={product.id} data={product} />)}
+        </section>
+      </Section>  
+    )  
+      
   );
 }
