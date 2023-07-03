65b10be5d6ab06ac2b0e08d8af68cc0c41268f27
Daiana de Paula <daianaadepaula1@gmail.com>
2023-07-03 08:16:38 -0300

    feat: style folder cart

--- src/components/Cart/cart.jsx
+++ src/components/Cart/cart.jsx
@@ -9,9 +9,7 @@ export default function Cart (){
   return (
     <section className="cart">
       <div className="cart-items">
-        {
-          cartItems.map((cartItem) => <CartItem key={cartItem.id} data={cartItem} />)
-        }
+        {cartItems.map((cartItem) => <CartItem key={cartItem.id} data={cartItem} />)}
       </div>
 
       <div className="cart-resume">resumo do carrinho</div>
