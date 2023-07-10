bc8b64b47e6985973a60c8df8ce0f770f08b5a72
Daiana de Paula <daianaadepaula1@gmail.com>
2023-07-10 12:04:42 -0300

    feat: remove cart items

--- src/components/CartItem/cartItem.jsx
+++ src/components/CartItem/cartItem.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import propTypes from 'prop-types';
 import './cartItem.css';
 
 import {BsCartDashFill} from 'react-icons/bs';
 import formatCurrency from '../../utils/formatCurrency';
+import AppContext from '../../context/AppContext';
 
 export default function CartItem({ data }){
 
-  const {thumbnail, title, price} = data;
+  const {cartItems, setCartItems} = useContext(AppContext);
+
+  const {id, thumbnail, title, price} = data;
+
+  const handleRemoveItem = () =>{
+    const updatedItems = cartItems.filter((item) => item.id !== id);
+    setCartItems(updatedItems);
+  };
 	
   return(
     <section className="cart-item">
@@ -20,7 +28,7 @@ export default function CartItem({ data }){
         <h3 className="cart-item-title">{title}</h3>
         <h3 className="cart-item-price">{formatCurrency(price, 'BRL')}</h3>
 
-        <button type="button" className="button__remove-item">
+        <button type="button" className="button__remove-item" onClick={ handleRemoveItem }>
           <BsCartDashFill />
         </button>
       </div>
